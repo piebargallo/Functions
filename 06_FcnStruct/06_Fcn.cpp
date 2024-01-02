44f58974b232e74796ea1cc989621eb65cae9193
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-01-02 12:29:19 +0100

    Add files via upload

---
+++ 06_FcnStruct/06_Fcn.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+struct S
+{
+    string name;
+    int num;
+};
+
+S g()
+{
+    string t{ "hello" };
+    int u{ 42 };
+    return { t, u };
+}
+
+int main()
+{
+    S s = g();
+    cout << s.name << " " << s.num << endl;
+    return 0;
+}
