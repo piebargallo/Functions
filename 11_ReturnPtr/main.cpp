b21a2888ed2fa0716af0d07ec420ed40c5eb2292
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-02-06 11:42:16 +0100

    Add files via upload

---
+++ 11_ReturnPtr/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int* max(int* a, int* b)
+{
+    return *a > *b ? a : b; // bien, puntero externo
+}
+
+int main()
+{
+    int a{ 11 }, b{ 14 };
+    std::cout << "Max: " << *max(&a, &b) << std::endl;
+    return 0;
+}
