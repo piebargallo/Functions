e50ab0c53fdbad71a163205450f279293aaf2cec
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-07-01 16:26:28 +0200

    Add files via upload

---
+++ ReturnByValue/ReturnByValue.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int Suma(int a, int b) {
+    return a + b;
+}
+
+int main()
+{
+    int resultado = Suma(4, 4);
+    cout << "Resultado: " << resultado << endl;
+    return 0;
+}
+
