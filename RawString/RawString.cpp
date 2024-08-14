a4731e4d3e74f3ff6fcc9137d1c6b74ac37caa1a
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-08-14 18:58:44 +0200

    Add files via upload

---
+++ RawString/RawString.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << R"stuff(c:\Program Files\World)stuff" << std::endl;
+	std::cout << R"chaiscript(c:\Program Files\World)chaiscript" << std::endl;
+	return 0;
+}
